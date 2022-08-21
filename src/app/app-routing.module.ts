e110fd2567e4b7d44f543b7f0cffac814b80f859
timhobbs <jesus.tesh@gmail.com>
2022-08-21 14:17:20 -0700

    add route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { HasConfigGuard } from 'src/app/guards/has-config.guard';
 import { HomeComponent } from 'src/app/home/home.component';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-    { path: 'table', loadChildren: () => import('./table/table.module').then(m => m.TableModule) },
-    { path: 'import', loadChildren: () => import('./import/import.module').then(m => m.ImportModule) },
-    { path: 'export', loadChildren: () => import('./export/export.module').then(m => m.ExportModule) },
+    { path: 'table', canActivate: [HasConfigGuard], loadChildren: () => import('./table/table.module').then(m => m.TableModule) },
+    { path: 'import', canActivate: [HasConfigGuard], loadChildren: () => import('./import/import.module').then(m => m.ImportModule) },
+    { path: 'export', canActivate: [HasConfigGuard], loadChildren: () => import('./export/export.module').then(m => m.ExportModule) },
     { path: 'configuration', loadChildren: () => import('./configuration/configuration.module').then(m => m.ConfigurationModule) },
     { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent },
