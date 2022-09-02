3d148b03279f5e81c62c440b7160f2b6e1dcf689
timhobbs <jesus.tesh@gmail.com>
2022-09-02 10:19:41 -0700

    use table svc in pipe for count

--- src/app/table/pipes/table-filter.pipe.ts
+++ src/app/table/pipes/table-filter.pipe.ts
@@ -1,20 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 import { Table } from 'src/app/table/table.interface';
+import { TableService } from 'src/app/services/table/table.service';
 
 @Pipe({
     name: 'tableFilter',
 })
 export class TableFilterPipe implements PipeTransform {
+    constructor(
+        private tableService: TableService,
+    ) {}
+
     transform(tables: Table[] | null, filter: string): Table[] {
         if (!tables) {
             return [];
         }
 
         if (!filter) {
+            this.tableService.setTableCount(tables.length);
+
             return tables;
         }
 
-        return tables.filter(t => t.description.toLocaleLowerCase().includes(filter));
+        const filtered = tables.filter(t => t.description.toLocaleLowerCase().includes(filter));
+        this.tableService.setTableCount(filtered.length);
+
+        return filtered;
     }
 }
