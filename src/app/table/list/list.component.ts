78e3fd85bc9889dd561e7d082f4eaa82162157f4
timhobbs <jesus.tesh@gmail.com>
2022-09-02 10:20:35 -0700

    show filtered table count

--- src/app/table/list/list.component.ts
+++ src/app/table/list/list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
-import { ApiService } from 'src/app/services/api.service';
+import { ApiService } from 'src/app/services/api/api.service';
 import { Observable } from 'rxjs';
 import { Table } from 'src/app/table/table.interface';
+import { TableService } from 'src/app/services/table/table.service';
 
 @Component({
     selector: 'app-list',
@@ -13,13 +14,17 @@ export class ListComponent implements OnInit {
     tables$: Observable<Table[]> | undefined;
     filter: string = '';
     viewType = 'list';
+    filteredTablesCount$: Observable<number> | undefined;
 
     constructor(
         private api: ApiService,
+        private tableService: TableService,
+        private changeDetectorRef: ChangeDetectorRef,
     ) {}
 
     ngOnInit(): void {
         this.tables$ = this.api.getTables();
+        this.filteredTablesCount$ = this.tableService.getTableCount();
     }
 
     clearFilter() {
@@ -28,5 +33,9 @@ export class ListComponent implements OnInit {
 
     setView(type: string) {
         this.viewType = type;
+    }
+
+    filterChanged() {
+        this.changeDetectorRef.detectChanges();
     }
 }
