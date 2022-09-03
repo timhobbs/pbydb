3d842210253a6710b048deece61dd3f0aac7cea3
timhobbs <jesus.tesh@gmail.com>
2022-09-03 01:32:17 -0700

    add socket.io to client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +11,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const config: SocketIoConfig = { url: 'http://localhost:3030', options: {} };
+
 @NgModule({
     declarations: [AppComponent, PageNotFoundComponent, HomeComponent],
     imports: [
@@ -18,6 +22,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
         HttpClientModule,
         MatToolbarModule,
         MatButtonModule,
+        SocketIoModule.forRoot(config),
     ],
     providers: [],
     bootstrap: [AppComponent],
