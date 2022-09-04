b3f12d841316a136b5a2478bee112017bac71a22
timhobbs <jesus.tesh@gmail.com>
2022-09-03 23:30:40 -0700

    import updates

--- src/app/import/import.component.ts
+++ src/app/import/import.component.ts
@@ -1,5 +1,6 @@
+import { Component, OnInit } from '@angular/core';
+
 import { ApiService } from 'src/app/services/api/api.service';
-import { Component } from '@angular/core';
 import { DbService } from 'src/app/services/db/db.service';
 import { Observable } from 'rxjs';
 import { Table } from 'src/app/table/table.interface';
@@ -9,9 +10,8 @@ import { Table } from 'src/app/table/table.interface';
     templateUrl: './import.component.html',
     styleUrls: ['./import.component.scss'],
 })
-export class ImportComponent {
+export class ImportComponent implements OnInit {
     results = 0;
-    vpsResults = 0;
     fileInput: File | null = null;
     dbTotal$: Observable<any> | undefined;
     dbStatus$: Observable<any> | undefined;
@@ -22,15 +22,18 @@ export class ImportComponent {
         private db: DbService,
     ) {}
 
-    import(type = 'vpx') {
-        const file = this.fileInput ?? null;
-        this.api.import(type, file).subscribe((tables: Table[]) => {
-            this.results = tables.length;
+    ngOnInit(): void {
+        this.dbTotal$ = this.db.getTotal();
+        this.dbStatus$ = this.db.getStatus();
+        this.db.getProcessedRecords().subscribe(processed => this.processed = processed);
+    }
 
-            if (type === 'vps') {
-                this.dbTotal$ = this.db.getTotal();
-                this.dbStatus$ = this.db.getStatus();
-                this.db.getProcessedRecords().subscribe(processed => this.processed = processed);
+    import(type = 'vpx') {
+
+        const file = this.fileInput ?? null;
+        this.api.import(type, file).subscribe((tables: any[]) => {
+            if (type === 'vpx') {
+                this.results = tables.length;
             }
         });
     }
